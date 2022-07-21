982e8d5ede95e9a08aa1434bf594bfd383338dde
yeonjoo.lee <teddyjune@naver.com>
2022-07-22 01:24:33 +0900

    add:ListTile 기능 추가

--- lib/future_listview.dart
+++ lib/future_listview.dart
@@ -13,18 +13,15 @@ class FutureListView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Colors.white,
-        title: const Text(
-          'Future Listview',
-          style: TextStyle(color: Colors.black),
-        ),
+        title: const Text('Future Listview'),
       ),
       body: FutureBuilder<List<Post>>(
-        future: getPosts(),
+        future: fetch(),
         builder: (context, snapshot) {
           if (snapshot.hasError) {
             return const Center(
               child: Text('에러가 발생했습니다'),
+              //에러가 발생하면 어떻게 해결해야 하나?
             );
           }
 
@@ -40,20 +37,32 @@ class FutureListView extends StatelessWidget {
             );
           }
 
-          final postData = snapshot.data!;
+          final recentData = snapshot.data!;
+          //이 snapshot 데이터는 뭐지?
+          // getPosts()함수의 리턴값인 posts가 리스트화 된건가?
 
-          if (postData.isEmpty) {
+          if (recentData.isEmpty) {
             return const Center(
               child: Text('데이터가 0개입니다'),
             );
           }
 
           return ListView.builder(
-            itemCount: postData.length,
-            itemBuilder: (context, int index) {
-              Post item = postData[index];
-              return ListTile(
-                title: Text(item.title),
+            itemCount: recentData.length,
+            itemBuilder: (context, index) {
+              Post item = recentData[index];
+              //여기에 index는 1,2,3, 등의 숫자인가?
+              //굳이 item을 선언해서 담아줘야 하나?
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  child: ListTile(
+                    leading: const Icon(Icons.wb_incandescent_outlined),
+                    title: Text(item.title),
+                    subtitle: Text(item.body),
+                    trailing: const Icon(Icons.menu),
+                  ),
+                ),
               );
             },
           );
@@ -62,13 +71,14 @@ class FutureListView extends StatelessWidget {
     );
   }
 
-  Future<List<Post>> getPosts() async {
+  Future<List<Post>> fetch() async {
     await Future.delayed(const Duration(seconds: 2));
 
     String jsonString = postData;
 
     Map<String, dynamic> json = jsonDecode(jsonString);
-    List title = json['title'];
-    return title.map((e) => Post.fromJson(e)).toList();
+    Iterable posts = json['posts'];
+    //데이터에 posts의 한 단계를 두지 않고 있는 그대로 가져올 방법은 없나?
+    return posts.map((e) => Post.fromJson(e)).toList();
   }
 }
